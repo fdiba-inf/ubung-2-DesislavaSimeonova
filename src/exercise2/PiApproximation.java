38275f768a217fdd63408247a2686fec7bddb65b
DesislavaSimeon <desislava.simeonova@fdiba.tu-sofia.bg>
2020-10-08 14:17:41 +0000

    change1

--- src/exercise2/PiApproximation.java
+++ src/exercise2/PiApproximation.java
@@ -1,11 +1,12 @@
 package exercise2;
 
+public class PiApproximation {
 
-public class PiApproximation{
- public static void main(String [] args){
-  double sum PI = 4.0*(1.0-1/3.0+1/5.0-1/7.0+1/11.0-1/13.0+1/17.0-1/19.0+1/23.0);
+  public static void main(String[]args) {
 
-  System.out.println("PI" + sum);
- }
+    double Pi = 4.0*(1.0-1.0/3.0+1.0/5.0-1.0/7.0+1.0/11.0-1.0/13.0+1.0/17.0-1.0/19.0+1.0/23.0);
 
+    System.out.println("Pi:" + Pi);
+     
+  }
 }
